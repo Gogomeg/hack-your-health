71bfdbff241bd7ad88abf245de5ca0145438e7ab
Gogomeg <>
2023-09-23 12:42:09 +0000

    Make migrations and apply not applied migration

--- todo/models.py
+++ todo/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_lenth=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
