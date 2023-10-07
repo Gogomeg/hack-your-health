54da1f1f91edc08287d2d8efdf202d675d50ee72
Gogomeg <>
2023-10-07 14:26:24 +0000

    Fixed alloed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hack-your-health-536c1472c698.herokuapp.com']
 
 
 # Application definition
