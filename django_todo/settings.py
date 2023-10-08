7b0d0a7846c1969f06aa777d222dc0f7b370eb23
Gogomeg <>
2023-10-08 11:42:07 +0000

    Import migrations to database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templated')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
