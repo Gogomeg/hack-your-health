4bdd1a8f7845b94caa47b3e606beabf40f316715
Gogomeg <>
2023-10-09 19:47:15 +0000

    Updated wsgy.py typing issue

--- django_stories/wsgi.py
+++ django_stories/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_sories.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_stories.settings')
 
 application = get_wsgi_application()
