fe0c74335b51967d70af73d982cab0f6cd715982
Gogomeg <>
2023-10-14 11:57:25 +0000

    Install Database and run migrations

--- django_stories/settings.py
+++ django_stories/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'django_stories.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+# # if development:
+# #     DATABASES = {
+# #         'default': {
+# #             'ENGINE': 'django.db.backends.sqlite3',
+# #             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# #         }
+# #     }
+# # else:
 
+DATABASES = {
+    ''default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
