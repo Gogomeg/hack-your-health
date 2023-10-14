fd134034bcccf8c762481bf61254466b90c8dad4
Gogomeg <>
2023-10-14 12:03:58 +0000

    Update setting.py

--- django_stories/settings.py
+++ django_stories/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'django_stories.wsgi.application'
 # # else:
 
 DATABASES = {
-    ''default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
