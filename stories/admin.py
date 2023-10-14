958498caa850a128343987ec3288542bb8a7be01
Gogomeg <>
2023-10-14 20:31:45 +0000

    Corrections

--- stories/admin.py
+++ stories/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from .models import Item
 from django_summernote.admin import SummernoteModelAdmin
 
 
--- stories/models.py
+++ stories/models.py
@@ -5,21 +5,14 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-
-
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
-    def __str__(self):
-        return self.name
-
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
